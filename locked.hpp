8a447f45f93f6cddbfcfb7c1c2687db8cddd9690
Joris Bayer <jjbayer@gmail.com>
2018-03-04 13:35:28 +0100

    Implement reference counting

--- locked.hpp
+++ locked.hpp
@@ -3,16 +3,72 @@
 #include <unordered_map>
 #include <mutex>
 
+/// Thread-safe mutex manager
+class MutexMap
+{
+public:
+
+    /// Create mutex if necessary, increase reference counter
+    std::mutex & getMutex(void * address)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        auto & entry = _map[address]; // Creates entry if does not exist
+        entry.counter++;
+
+        return entry.mutex;
+    }
+
+    /// Decrease reference counter, delete mutex if necessary
+    void leaveMutex(void * address)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        const auto it = _map.find(address);
+        auto & entry = it->second; // must exist
+        if( --entry.counter == 0) {
+            _map.erase(it);
+        }
+    }
+
+private:
+
+    struct CountingMutex
+    {
+        std::mutex mutex;
+        int counter = 0;
+    };
+
+    std::mutex _mutex;
+    std::unordered_map<void *, CountingMutex> _map;
+};
+
+
 template<typename T>
 class LockedObject
 {
     public:
 
         LockedObject(T & object)
-            :_lock(getMutex(object))
-            ,_object(&object)
+            :_object(&object)
+            ,_lock(getMutexMap().getMutex(_object))
         {
 
+        }
+
+        /// Move constructor
+        LockedObject(LockedObject && other)
+            :_object(nullptr)
+            ,_lock()
+        {
+            std::swap(_object, other._object);
+            std::swap(_lock, other._lock);
+        }
+
+        ~LockedObject()
+        {
+            _lock.unlock();
+            getMutexMap().leaveMutex(_object);
         }
 
     public:
@@ -20,20 +76,16 @@ class LockedObject
 
     private:
 
-        std::mutex & getMutex(const T & object)
+        /// Class-wide mutex map
+        MutexMap & getMutexMap()
         {
-            static std::unordered_map<const void*, std::mutex> objectMutexes;
-            static std::mutex mapMutex;
+            static MutexMap map;
 
-            // Only one object of type T can access the mutex map at a time
-            std::lock_guard<std::mutex> mapLock(mapMutex);
-
-            // TODO: refcount mutexes, delete when done
-            return objectMutexes[static_cast<const void*>(&object)];
+            return map;
         }
 
+        T * _object;
         std::unique_lock<std::mutex> _lock;
-        T *const _object;
 };
 
 
